8cca4c2347572d9d24577f79bac0492919eb0934
Phuc Hau <nhojsieu122@gmail.com>
2023-02-18 13:59:35 +0700

    first commit

---
+++ darknet/lpr.py
@@ -0,0 +1,140 @@
+import sys, os, time
+import keras
+import cv2
+import numpy as np
+import traceback
+import argparse
+import darknet
+from wpod_src.keras_utils 			import load_model
+from os.path 					    import splitext, basename
+from wpod_src.utils 				import im2single
+from wpod_src.keras_utils 			import load_model, detect_lp
+
+
+
+def resize_bbox(detections, out_size, in_size):
+
+    detections = list(detections)
+    for det in detections:
+        det = list(det)
+        det[2] = list(det[2])
+
+        xmin, ymin, xmax, ymax = darknet.bbox2points(det[2])
+        y_scale = float(out_size[0]) / in_size[0]
+        x_scale = float(out_size[1]) / in_size[1]
+        ymin = int(y_scale * ymin)
+        ymax = int(y_scale * ymax)
+        xmin = int(x_scale * xmin) if int(x_scale * xmin) > 0 else 0
+        xmax = int(x_scale * xmax)
+
+        det[2] = [xmin, ymin, xmax, ymax]
+
+    detections = sorted(detections, key = lambda x: x[2][1])
+    line = detections[0][2][3]
+    begin_idx = 0 
+    for i, det in enumerate(detections):
+        if det[2][1] > line:
+            detections[begin_idx:i] = sorted(detections[begin_idx:i], key = lambda x: x[2][0])
+            begin_idx = i 
+            break
+    detections[begin_idx:] = sorted(detections[begin_idx:], key = lambda x: x[2][0])
+
+    return detections
+
+
+def character_detect(frame, threshold, network, class_names, class_colors):
+ 
+    prev_time = time.time()
+
+    width = darknet.network_width(network)
+    height = darknet.network_height(network)
+    darknet_image = darknet.make_image(width, height, 3)
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image_resized = cv2.resize(image_rgb, (width, height), 
+                                interpolation=cv2.INTER_LINEAR)
+
+    darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
+    detections = darknet.detect_image(network, class_names, darknet_image, thresh=threshold)
+    darknet.free_image(darknet_image)
+    
+    image = darknet.draw_boxes(detections, image_resized, class_colors)
+    print(detections)
+    darknet.print_detections(detections)
+
+    det_time = time.time() - prev_time
+    fps = int(1/(time.time() - prev_time))
+    print("Detection time: {}".format(det_time))
+    print("FPS: {}".format(fps))
+
+    out_size = frame.shape[:2]
+    in_size = image_resized.shape[:2]
+    detections = resize_bbox(detections, out_size, in_size)
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), detections
+
+
+def parser():
+    parser = argparse.ArgumentParser(description="License Plate Recognition")
+    parser.add_argument("--input_dir", type=str, default="", help="input")
+    parser.add_argument("--output_dir", default = "./output", help="output")
+    parser.add_argument("--lp_threshold", default = .5, help="lp-threshold")
+    parser.add_argument("--threshold", default = .5, help="threshold")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parser()
+    wpod_net_path = './models/my-trained-model/wpod_ver4_backup.h5'
+    lp_threshold = args.lp_threshold
+
+    config_file = './cfg/yolov4-tiny-mish.cfg'
+    data_file = './data/obj.data'
+    weights = './backup/yolov4-tiny-mish_best.weights'
+    thresh = args.threshold
+
+    img_path = args.input_dir
+    output_dir = args.output_dir
+
+    wpod_net = load_model(wpod_net_path)
+    network, class_names, class_colors = darknet.load_network(config_file, data_file, weights)
+
+    print ('Searching for license plates using WPOD-NET')
+
+    bname = splitext(basename(img_path))[0]
+    Ivehicle = cv2.imread(img_path)
+    ratio = float(max(Ivehicle.shape[:2]))/min(Ivehicle.shape[:2])
+    side  = int(ratio*288.)
+    bound_dim = min(side + (side%(2**4)),608)
+    
+    Llp,LlpImgs,_ = detect_lp(wpod_net,im2single(Ivehicle),bound_dim,2**4,(240,80),lp_threshold)
+
+    if len(LlpImgs):
+        Ilp = LlpImgs[0]
+        # cv2.imshow('crop', Ilp)
+
+    text = []
+    if len(Ilp): 
+        Ilp = np.array(Ilp*255.0, dtype=np.uint8)
+        image, detections = character_detect(Ilp, thresh, network, class_names, class_colors)
+
+        for det in detections:
+            text.append(det[0])
+        text = ''.join(text)
+        print(text)
+        # cv2.imshow('image', image)
+
+    cv2.imwrite('%s/%s_output.jpg' % (output_dir, bname), image)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+
+    try:
+        main()		 
+    except:
+        traceback.print_exc()
+        sys.exit(1)
+
+    sys.exit(0)
+
+
---
+++ darknet/wpod_src/keras_utils.py
@@ -0,0 +1,162 @@
+
+import numpy as np
+import cv2
+import time
+
+from os.path import splitext
+
+from wpod_src.label import Label
+from wpod_src.utils import getWH, nms
+from wpod_src.projection_utils import getRectPts, find_T_matrix
+
+
+class DLabel (Label):
+
+	def __init__(self,cl,pts,prob):
+		self.pts = pts
+		tl = np.amin(pts,1)
+		br = np.amax(pts,1)
+		Label.__init__(self,cl,tl,br,prob)
+
+def save_model(model,path,verbose=0):
+	path = splitext(path)[0]
+	model_json = model.to_json()
+	with open('%s.json' % path,'w') as json_file:
+		json_file.write(model_json)
+	model.save_weights('%s.h5' % path)
+	if verbose: print ('Saved to %s' % path)
+
+def load_model(path,custom_objects={},verbose=0):
+	from keras.models import model_from_json
+
+	path = splitext(path)[0]
+	with open('%s.json' % path,'r') as json_file:
+		model_json = json_file.read()
+	model = model_from_json(model_json, custom_objects=custom_objects)
+	model.load_weights('%s.h5' % path)
+	if verbose: print ('Loaded from %s' % path)
+	return model
+
+def order_points(pts):
+	# initialize a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype = "float32")
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = np.sum(pts,axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	# return the ordered coordinates
+	return rect
+
+def reconstruct(Iorig,I,Y,out_size,threshold=.9):
+	net_stride 	= 2**4
+	side 		= ((208. + 40.)/2.)/net_stride # 7.75
+
+	Probs = Y[...,0]
+	Affines = Y[...,2:]
+	rx,ry = Y.shape[:2]
+	ywh = Y.shape[1::-1]
+	iwh = np.array(I.shape[1::-1],dtype=float).reshape((2,1))
+
+	xx,yy = np.where(Probs>threshold)
+
+	WH = getWH(I.shape)
+	MN = WH/net_stride
+
+	vxx = vyy = 0.5 #alpha
+
+	base = lambda vx,vy: np.matrix([[-vx,-vy,1.],[vx,-vy,1.],[vx,vy,1.],[-vx,vy,1.]]).T
+	labels = []
+
+	for i in range(len(xx)):
+		y,x = xx[i],yy[i]
+		affine = Affines[y,x]
+		prob = Probs[y,x]
+
+		mn = np.array([float(x) + .5,float(y) + .5])
+
+		A = np.reshape(affine,(2,3))
+		A[0,0] = max(A[0,0],0.)
+		A[1,1] = max(A[1,1],0.)
+
+		pts = np.array(A*base(vxx,vyy)) #*alpha
+		# print(pts)
+		pts_MN_center_mn = pts*side
+		pts_MN = pts_MN_center_mn + mn.reshape((2,1))
+
+		pts_prop = pts_MN/MN.reshape((2,1))
+
+		labels.append(DLabel(0,pts_prop,prob))
+
+	final_labels = nms(labels,.1)
+	# print(final_labels)
+	TLps = []
+
+	if len(final_labels):
+		final_labels.sort(key=lambda x: x.prob(), reverse=True)
+		for i,label in enumerate(final_labels):
+
+			# t_ptsh 	= getRectPts(0,0,out_size[0],out_size[1])
+			
+			# ptsh 	= np.concatenate((label.pts*getWH(Iorig.shape).reshape((2,1)),np.ones((1,4))))
+			# H 		= find_T_matrix(ptsh,t_ptsh)
+			
+			pts = label.pts*getWH(Iorig.shape).reshape((2,1))
+			ptss = (pts[0][0], pts[1][0]), (pts[0][1], pts[1][1]), (pts[0][2], pts[1][2]), (pts[0][3], pts[1][3])
+			# print(ptss)
+
+			rect = order_points(ptss)
+			(tl, tr, br, bl) = rect
+      # compute the width of the new image, which will be the
+      # maximum distance between bottom-right and bottom-left
+      # x-coordiates or the top-right and top-left x-coordinates
+			widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+			widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+			maxWidth = max(int(widthA), int(widthB))
+      # compute the height of the new image, which will be the
+      # maximum distance between the top-right and bottom-right
+      # y-coordinates or the top-left and bottom-left y-coordinates
+			heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+			heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+			maxHeight = max(int(heightA), int(heightB))
+
+			dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+      # compute the perspective transform matrix and then apply it
+			M = cv2.getPerspectiveTransform(rect, dst)
+
+			Ilp 	= cv2.warpPerspective(Iorig,M,(maxWidth, maxHeight),borderValue=.0)
+
+			TLps.append(Ilp)
+
+	return final_labels,TLps
+	
+def detect_lp(model,I,max_dim,net_step,out_size,threshold):
+
+	min_dim_img = min(I.shape[:2])
+	factor 		= float(max_dim)/min_dim_img
+
+	w,h = (np.array(I.shape[1::-1],dtype=float)*factor).astype(int).tolist()
+	w += (w%net_step!=0)*(net_step - w%net_step)
+	h += (h%net_step!=0)*(net_step - h%net_step)
+	Iresized = cv2.resize(I,(w,h))
+
+	T = Iresized.copy()
+	T = T.reshape((1,T.shape[0],T.shape[1],T.shape[2]))
+
+	start 	= time.time()
+	Yr 		= model.predict(T)
+	Yr 		= np.squeeze(Yr)
+	elapsed = time.time() - start
+
+	L,TLps = reconstruct(I,Iresized,Yr,out_size,threshold)
+
+	return L,TLps,elapsed
